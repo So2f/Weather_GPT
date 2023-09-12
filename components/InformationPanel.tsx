4b4b70afb29747cae15fa3ff9fdafd1af35d12a5
So2f <soffiane.elkarouni@gmail.com>
2023-09-12 20:14:38 +0200

    test

--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -91,7 +91,7 @@ function InformationPanel({ city, lat, long, results }: Props) {
         <div className="flex items-center space-x-2 px-4 py-3 border border-[#6F90CD] rounded-md bg-[#405885]">
           <MoonIcon className="h-10 w-10 text-gray-400" />
           <div className="flex-1 flex justify-between items-center">
-            <p className="font-extralight">Sunset</p>
+            <p className="font-extralight">sunset</p>
             <p className="uppercase text-2xl">
               {new Date(results.daily.sunset[0]).toLocaleTimeString("en-GB", {
                 hour: "numeric",
