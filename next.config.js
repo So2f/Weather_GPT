a679e909181ec4f959d587783399ee31260abd5d
So2f <soffiane.elkarouni@gmail.com>
2023-09-12 18:51:29 +0200

    1st push

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["www.weatherbit.io"],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
